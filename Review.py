59f826d87df25da178ab115df3b56070e201f950
zakariamohamed123 <149509331+zakariamohamed123@users.noreply.github.com>
2023-12-18 12:58:51 +0300

    Update Review.py

--- Review.py
+++ Review.py
@@ -1,19 +1,18 @@
 class Review:
-    all_reviews = []
-
-    def __init__(self, customer, restaurant, rating):
-        self.customer = customer
-        self.restaurant = restaurant
+    reviews=[]
+    def __init__(self, customer, restuarant, rating):
+        self._customer = customer
+        self._restaurant = restuarant
         self.rating = rating
-        Review.all_reviews.append(self)
-
+        Review.reviews.append(self)
     def rating(self):
         return self.rating
-
-    def __str__(self):
-        return f"Review: {self.customer.full_name()} rated {self.rating} stars"
-
-    @classmethod
     def all(cls):
-        return cls.all_reviews
-      
+        return cls.reviews
+    def customer(self):
+        return self._customer
+    def restaurant(self):
+        return self._restaurant
+
+first_rating= Review('Gabrielle', 'La Bodega', 10)
+print(first_rating.rating)
